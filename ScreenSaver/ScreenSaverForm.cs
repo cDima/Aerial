45123fa440628b776b85005f2b0dc9f249551c62
Alistair McMillan <alistairmcmillan75@gmail.com>
2016-10-15 18:06:52 +0100

    Catch IOException if it does still occur
    Rather than an error popping up on screen.

--- ScreenSaver/ScreenSaverForm.cs
+++ ScreenSaver/ScreenSaverForm.cs
@@ -144,7 +144,13 @@ namespace ScreenSaver
         private void OnDownloadFileComplete(object sender, AsyncCompletedEventArgs e)
         {
             if (e.Cancelled == false && e.Error == null) {
-	            Directory.Move(Path.Combine(tempFolder, e.UserState.ToString()), Path.Combine(cacheFolder, e.UserState.ToString()));
+                try
+                {
+                    Directory.Move(Path.Combine(tempFolder, e.UserState.ToString()), Path.Combine(cacheFolder, e.UserState.ToString()));
+                }
+                catch (IOException ioe)
+                {
+                }
             }
         }
 
