c6f753ac262aac360ce6db8f983398a96372017f
Dmitry Sadakov <sadakov@gmail.com>
2017-02-11 11:58:12 -0500

    #94 Supporting offline entites download.
    Use compile flag OFFLINE to model entities download errros.

--- ScreenSaver/AerialEntities.cs
+++ ScreenSaver/AerialEntities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,16 +15,13 @@ namespace Aerial
 
         public List<Asset> GetMovies()
         {
-            var settings = new RegSettings();
-            
-            var aerialUrl = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json";
-            WebClient webClient = new WebClient();
-            string ee = webClient.DownloadString(aerialUrl);
+            var entries = GetEntries();
 
-            var urls = new JavaScriptSerializer().Deserialize<IdAsset[]>(ee);
+            var urls = new JavaScriptSerializer().Deserialize<IdAsset[]>(entries);
 
             var time = (DateTime.Now.Hour < 6 || DateTime.Now.Hour > 19) ? "night" : "day";
             var ran = new Random();
+            var settings = new RegSettings();
             var links = urls.SelectMany(s => s.assets)
                 .OrderBy(t => ran.Next())
                 .OrderByDescending(t => settings.UseTimeOfDay && t.timeOfDay == time)
@@ -35,6 +33,24 @@ namespace Aerial
                 cachedPlaylist = links;
 
             return cachedPlaylist;
+        }
+
+        private string GetEntries()
+        {
+            var aerialUrl = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json";
+#if OFFLINE
+            aerialUrl = "http://BOGUS/entries.json";
+#endif
+
+            // update anyway
+            Caching.StartDelayedCache(aerialUrl); 
+
+            if (Caching.IsHit(aerialUrl))
+                return File.ReadAllText(Caching.Get(aerialUrl));
+
+            WebClient webClient = new WebClient();
+            string entries = webClient.DownloadString(aerialUrl);
+            return entries;
         }
     }
 
